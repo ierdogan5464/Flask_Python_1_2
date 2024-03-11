a7b627719798e0e5b54a80d4b8e62ce1139affbf
Ishak <ierdogan5464@gmail.com>
2024-03-11 23:37:22 +0300

    Second push

--- flask-01-hello-world-app/hello-world-app.py
+++ flask-01-hello-world-app/hello-world-app.py
@@ -1,12 +1,22 @@
 from flask import Flask 
 app = Flask(__name__)
 
+@app.route("/")
+def head ():
+    return "hello world clarusway 17"
 
+@app.route("/second")
+def head2 ():
+    return "Second page"
+
+@app.route("/third")
+def head3 ():
+    return "third page"
 
 
 
 
 if __name__ == '__main__':
 
-    app.run(debug=True, port=30000)
-    # app.run(host= '0.0.0.0', port=8081)
+    #  app.run(debug=True, port=30000)
+    app.run(host= '34.201.46.238', port=8081)
